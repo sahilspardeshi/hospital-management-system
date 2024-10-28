52b14554839911cefabbd526c6b7ecc4d21a6042
Bhoyate Dhanashree <bhoyatedhanashree5055@gmail.com>
2024-10-29 00:13:36 +0530

    testing medical components

--- client/src/App/components/Medication/Medication.jsx
+++ client/src/App/components/Medication/Medication.jsx
@@ -1,6 +1,5 @@
-//Main file Of Medication 
 import React, { useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import MedicationList from '../Medication/MedicationList';
 import NewMedicationForm from '../Medication/NewMedicationForm';
 import MedicationDetails from '../Medication/MedicationDetails';
@@ -14,33 +13,24 @@ const Medication = () => {
 
   return (
     <div className="min-h-screen bg-custom-gradient flex">
-    {/* Sidebar */}
-    <div className="bg-[#F8F7F7] bg-opacity-70 shadow-lg rounded-xl border border-gray-200 w-full  max-w-full max-h-full overflow-y-auto flex flex-row justify-start items-start mx-5 my-5 px-5 py-5">
-    <Sidebar/>
-    <div className="flex-1 flex flex-col">
-    <Navbar/>
+      {/* Sidebar */}
+      <Sidebar />
+      <div className="flex-1 flex flex-col bg-[#F8F7F7] bg-opacity-70 shadow-lg rounded-xl border border-gray-200 mx-5 my-5 overflow-hidden">
+        <Navbar />
         {/* Main Content - Medication Management */}
-        <div className="flex-1 p-6 bg-white rounded-lg shadow-md m-4 ">
-        <div className="bg-white  p-4 ">
-        
-        </div>
-        
-
+        <div className="flex-1 p-6 bg-white rounded-lg shadow-md m-4">
           <MedicationContext.Provider value={{ medications, setMedications }}>
-            <Router>
-              <div className="container mx-auto">
-                <Routes>
-                  <Route path="/" element={<MedicationList />} />
-                  <Route path="/new-medication" element={<NewMedicationForm />} />
-                  <Route path="/medication-details" element={<MedicationDetails />} />
-                  <Route path="/edit-medication/:id" element={<MedicationDetails />} />
-                </Routes>
-              </div>
-            </Router>
+            <div className="container mx-auto">
+              <Routes>
+                <Route path="/" element={<MedicationList />} />
+                <Route path="/new-medication" element={<NewMedicationForm />} />
+                <Route path="/medication-details" element={<MedicationDetails />} />
+                <Route path="/edit-medication/:id" element={<MedicationDetails />} />
+              </Routes>
+            </div>
           </MedicationContext.Provider>
         </div>
       </div>
-    </div>
     </div>
   );
 };
