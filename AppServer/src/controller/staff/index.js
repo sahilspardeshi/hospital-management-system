3e4d7bc3661205c9e774afac165d64dfcc8a79e6
Bhoyate Dhanashree <bhoyatedhanashree5055@gmail.com>
2024-11-21 10:43:39 +0530

    fetch login user controller

--- AppServer/src/controller/staff/index.js
+++ AppServer/src/controller/staff/index.js
@@ -5,5 +5,7 @@ import { getAllStaff,getStaffById ,getStaffByName} from "./GET/get.staff.js";
 import { login } from "./auth/Login.js";
 import { logout } from "./auth/Logout.js";
 import { refreshToken } from "./auth/refreshToken.js";
+import { getProfile } from "./auth/getprofile.js";
+import { authenticateToken } from "../../middleware/verify.js";
 
-export {createStaff,deleteStaff,getAllStaff,getStaffById,updateStaff,login,logout,refreshToken,getStaffByName};
+export {createStaff,deleteStaff,getAllStaff,getStaffById,updateStaff,login,logout,refreshToken,getStaffByName ,getProfile , authenticateToken};
--- client/src/App/redux/actions/loginActions.js
+++ client/src/App/redux/actions/loginActions.js
@@ -8,7 +8,9 @@ export const LOGIN_REQUEST = 'LOGIN_REQUEST';
 export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
 export const LOGIN_FAILURE = 'LOGIN_FAILURE';
 
-
+export const GET_LOGIN_REQUEST = 'GET_LOGIN_REQUEST';
+export const GET_LOGIN_SUCCESS = 'GET_LOGIN_SUCCESS';
+export const GET_LOGIN_FAILURE = 'GET_LOGIN_FAILURE';
 
 export const login = (userId, password, onSuccess) => {
   return async (dispatch) => {
@@ -24,6 +26,28 @@ export const login = (userId, password, onSuccess) => {
     } catch (error) {
       const errorMsg = error.response?.data?.message || error.message;
       dispatch({ type: LOGIN_FAILURE, payload: errorMsg });
+    }
+  };
+};
+
+
+export const getLoginProfile = (onSuccess) => {
+  return async (dispatch) => {
+    dispatch({ type: GET_LOGIN_REQUEST });
+
+    try {
+      
+      const response = await axiosInstanceApp.get('auth/profile');
+      
+      dispatch({ type: GET_LOGIN_SUCCESS, payload: response.data });
+
+      
+      if (onSuccess) {
+        onSuccess(response.data);
+      }
+    } catch (error) {
+      const errorMsg = error.response?.data?.message || error.message;
+      dispatch({ type: GET_LOGIN_FAILURE, payload: errorMsg });
     }
   };
 };
