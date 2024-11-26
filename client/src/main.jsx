81648e5fab3b7955436054a589b6843ddfd864ab
BhaskarT18 <bhaskartikale@gmail.com>
2024-11-26 12:40:45 +0530

    fix store file

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,22 +3,12 @@ import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './App.jsx';
 import './index.css';
-import appStore from './App/redux/store.js'; // App store
-import websiteStore from './website/redux/store.js'; // Website store
 import { ToastContainer } from 'react-toastify';
 
-const hostname = window.location.hostname;
-
-// Check if we're on localhost, custom domain, or production domain
-const isLocalHost = hostname === 'localhost';
-const isProductionDomain = hostname === 'example.com'; // Actual production domain
-
-// Choose the store based on the condition
-const selectedStore = isLocalHost || isProductionDomain ? websiteStore : appStore;
-
+import store from './store.js'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Provider store={selectedStore}>
+    <Provider store={store}>
       <App />
       <ToastContainer />
     </Provider>
---
+++ client/src/store.js
@@ -0,0 +1,32 @@
+// src/app/redux/store.js
+import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers } from 'redux';
+
+//app
+import { patientReducer } from './App/redux/reducers/patientReducer';
+import { loginReducer } from './App/redux/reducers/loginReducer';
+import { ProfileReducer } from './App/redux/reducers/StaffProfileReducer';
+
+//web
+import { accountReducer } from './website/redux/reducers/accountReducer'; // Ensure this path is correct
+import { adReducer } from './website/redux/reducers/adReducer';
+
+
+const rootReducer = combineReducers({
+    login: loginReducer,
+    patient: patientReducer,
+    profile: ProfileReducer,
+    account: accountReducer,
+    advertisement: adReducer,
+    
+  });
+  
+  
+  // Create the Redux store
+  const store = configureStore({
+    reducer: rootReducer,
+    // Middleware can be added if needed
+  });
+  
+  export default store;
+  
